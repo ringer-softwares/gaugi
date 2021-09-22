73529e3488d179939fc0efa88c7977d8fe74761f
jodafons <jodafons@lps.ufrj.br>
2021-09-22 20:01:36 -0300

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'Gaugi',
-  version = '1.0.23',
+  version = '1.0.00',
   license='GPL-3.0',
   description = '',
   long_description = long_description,
